7a4dd2b96f8f9f349ef2ced4448c5f9a6faca2c3
sdl60660 <learnersd@gmail.com>
2020-12-01 16:02:00 -0500

    "Refactored code to prepare it for use on a flask server"

--- data_processing/build_model.py
+++ data_processing/build_model.py
@@ -14,31 +14,36 @@ from config import config
 from get_user_ratings import get_user_data
 
 
-# Load ratings data
-df = pd.read_csv('data/sample_rating_data.csv')
-# print(df.head())
+def build_model(username):
+    # Load ratings data
+    df = pd.read_csv('data/training_data.csv')
+    # print(df.head())
+ 
+    user_movies = get_user_data(username)
+    user_rated = [x for x in user_movies if x['rating_val'] > 0]
 
-username = "faycwalker"
-user_movies = get_user_data(username)
-user_rated = [x for x in user_movies if x['rating_val'] > 0]
+    user_df = pd.DataFrame(user_rated)
+    df = pd.concat([df, user_df]).reset_index(drop=True)
+    df.drop_duplicates(inplace=True)
 
-user_df = pd.DataFrame(user_rated)
-df = pd.concat([df, user_df]).reset_index(drop=True)
-df.drop_duplicates(inplace=True)
+    # Surprise dataset loading
+    reader = Reader(rating_scale=(1, 10))
+    data = Dataset.load_from_df(df[["user_id", "movie_id", "rating_val"]], reader)
 
-# Surprise dataset loading
-reader = Reader(rating_scale=(1, 10))
-data = Dataset.load_from_df(df[["user_id", "movie_id", "rating_val"]], reader)
+    # Configure algorithm
+    algo = SVD()
+    # cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
 
-# Configure algorithm
-algo = SVD()
-# cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+    trainingSet = data.build_full_trainset()
+    algo.fit(trainingSet)
 
-trainingSet = data.build_full_trainset()
-algo.fit(trainingSet)
+    user_watched_list = [x['movie_id'] for x in user_movies]
 
-dump("models/mini_model.pkl", predictions=None, algo=algo, verbose=1)
+    return algo, user_watched_list
 
-user_watched_list = [x['movie_id'] for x in user_movies]
-with open("models/user_watched.txt", "wb") as fp:
-    pickle.dump(user_watched_list, fp)
+if __name__ == "__main__":
+    algo, user_watched_list = build_model("samlearner")
+
+    dump("models/mini_model.pkl", predictions=None, algo=algo, verbose=1)
+    with open("models/user_watched.txt", "wb") as fp:
+        pickle.dump(user_watched_list, fp)
---
+++ data_processing/run_model.py
@@ -0,0 +1,49 @@
+#!/usr/local/bin/python3.9
+
+from collections import defaultdict
+
+from surprise import Dataset
+from surprise import SVD
+from surprise import Reader
+from surprise.model_selection import GridSearchCV
+from surprise import SVD
+from surprise.dump import load
+
+import pickle
+
+import pandas as pd
+
+
+def run_model(username, algo, user_watched_list, threshold_movie_list, num_recommendations=20):
+    
+    def get_top_n(predictions, n=20):
+        top_n = [(iid, est) for uid, iid, true_r, est, _ in predictions]
+        top_n.sort(key=lambda x: x[1], reverse=True)
+
+        return top_n[:n]
+
+    unwatched_movies = [x for x in threshold_movie_list if x not in user_watched_list]
+    prediction_set = [(username, x, 0) for x in unwatched_movies]
+
+    # prediction = algo.predict(username, "despicable-me-3")
+    # print(prediction.est)
+
+    predictions = algo.test(prediction_set)
+    top_n = get_top_n(predictions, num_recommendations)
+
+    # Print the recommended items for user
+    for prediction in top_n:
+        print(f"{prediction[0]}: {round(prediction[1], 2)}")
+    
+    return top_n
+
+if __name__ == "__main__":
+    with open("models/user_watched.txt", "rb") as fp:
+        user_watched_list = pickle.load(fp)
+
+    with open("models/threshold_movie_list.txt", "rb") as fp:
+        threshold_movie_list = pickle.load(fp)
+
+    algo = load("models/mini_model.pkl")[1]
+
+    run_model("samlearner", algo, user_watched_list, threshold_movie_list, 25)
