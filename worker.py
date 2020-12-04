c6ac2b08bb1d8ccd8d39e68f9174f06d1132dc69
sdl60660 <learnersd@gmail.com>
2020-12-04 17:53:00 -0500

    Changed default model strength

--- worker.py
+++ worker.py
@@ -1,19 +1,23 @@
 import os
 
 import redis
+from redis import Redis
 from rq import Worker, Queue, Connection
+from rq.command import send_kill_horse_command
 
-from autoworker import AutoWorker
+# from autoworker import AutoWorker
 
 
 listen = ['high', 'default', 'low']
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
+redis = Redis()
 
 
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(Queue, listen))
+        send_kill_horse_command(redis, worker.name)
         worker.work()
 
         # for channel in listen:
