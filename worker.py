777f5a5636f7701de84512f8d1c2958e743bef90
sdl60660 <learnersd@gmail.com>
2020-12-04 17:58:19 -0500

    Made a change to worker file

--- worker.py
+++ worker.py
@@ -1,29 +1,20 @@
 import os
 
 import redis
-from redis import Redis
+# from redis import Redis
 from rq import Worker, Queue, Connection
-from rq.command import send_kill_horse_command
-
-# from autoworker import AutoWorker
+# from rq.command import send_kill_horse_command
 
 
 listen = ['high', 'default', 'low']
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
-redis = Redis()
+# redis = Redis()
 
 
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(Queue, listen))
-        send_kill_horse_command(redis, worker.name)
+        # send_kill_horse_command(redis, worker.name)
         worker.work()
-
-        # for channel in listen:
-        #     worker = Worker([Queue(channel)], connection=conn, name=f"{channel}_worker")
-        #     worker.work()
-        
-        # aw = AutoWorker(map(Queue, listen), max_procs=6, skip_failed=False)
-        # aw.work()
         
