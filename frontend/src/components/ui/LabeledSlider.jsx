26d44f385e576fb548b5d005e34d41aaf6b115dc
sdl60660 <learnersd@gmail.com>
2021-12-27 01:02:02 -0500

    Updated mobile styling

--- frontend/src/components/ui/LabeledSlider.jsx
+++ frontend/src/components/ui/LabeledSlider.jsx
@@ -16,13 +16,13 @@ const LabeledSlider = (props) => {
             <Grid item className="slider-end-label slider-end-label--left">
                 <FormLabel>{labels[0]}</FormLabel>
             </Grid>
-            <Grid item xs>
-            <Slider
-                defaultValue={defaultValue}
-                valueLabelDisplay={valueLabelDisplay}
-                marks={marks}
-                {...props}
-            />
+            <Grid item xs className="slider-wrapper">
+                <Slider
+                    defaultValue={defaultValue}
+                    valueLabelDisplay={valueLabelDisplay}
+                    marks={marks}
+                    {...props}
+                />
             </Grid>
             <Grid item  className={"slider-end-label slider-end-label--right"}>
                 <FormLabel>{labels[1]}</FormLabel>
