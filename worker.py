98b566011ab463c67f0ae337b7fdcdf45cc023bd
sdl60660 <learnersd@gmail.com>
2022-08-09 19:00:24 -0400

    Fixed another bug! Ahhh, not being able to run in the same redis environment locally makes debugging really tought

--- worker.py
+++ worker.py
@@ -11,7 +11,7 @@ listen = ['high', 'default', 'low']
 # redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis_url = 'redis://localhost:6379'
 if os.getenv('REDISCLOUD_URL'):
-    redis_url = urlparse.urlparse(os.getenv('REDISCLOUD_URL'))
+    redis_url = urlparse(os.getenv('REDISCLOUD_URL'))
 
 print('here', redis_url, os.getenv('REDISCLOUD_URL'))
 
