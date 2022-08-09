6eb80e1f167dad464093f96b0138bde030324f1e
Sam Learner <learnersd@gmail.com>
2022-08-09 18:37:24 -0400

    Trying Redis update

--- worker.py
+++ worker.py
@@ -1,5 +1,5 @@
 import os
-
+import urlparse
 import redis
 # from redis import Redis
 from rq import Worker, Queue, Connection
@@ -7,15 +7,16 @@ from rq import Worker, Queue, Connection
 
 
 listen = ['high', 'default', 'low']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-conn = redis.from_url(redis_url)
-# redis = Redis()
 
+# redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = 'redis://localhost:6379'
+if os.getenv('REDISTOGO_URL'):
+    redis_url = urlparse.urlparse(os.getenv('REDISCLOUD_URL'))
+
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
         # send_kill_horse_command(redis, worker.name)
-        
-        
