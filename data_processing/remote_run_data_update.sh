baacb5381ae26ccabfd5d6d4ec4d6132da792e3c
sdl60660 <learnersd@gmail.com>
2023-01-04 21:09:56 -0500

    Updating remote script

--- data_processing/remote_run_data_update.sh
+++ data_processing/remote_run_data_update.sh
@@ -1,11 +1,18 @@
+# Sync from Github
 git pull origin main
 
+# Run DB updates
 pipenv run python get_users.py
 pipenv run python get_ratings.py
 pipenv run python get_movies.py
 
+# Generate new training data file
 pipenv run python create_training_data.py
 
+# Push to Github
 git add .
 git commit -m "Automated data update from EC2"
 git push origin main
+
+# Shutdown remote EC2 instance when finished
+sudo shutdown -h now
