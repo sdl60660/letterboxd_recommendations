7ef840ced6cdbd58e222caa5bd99b3242826ff83
Sam Learner <sam.learner@ft.com>
2023-04-23 23:36:18 -0400

    trying to push a redirect on heroku homepage to samlearner.com domain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, status
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
@@ -30,7 +30,7 @@ origins = [
     "http://letterboxd-recommendations.herokuapp.com",
     "https://letterboxd-recommendations.herokuapp.com",
     "http://letterboxd.samlearner.com",
-    "https://letterboxd.samlearner.com"
+    "https://letterboxd.samlearner.com",
 ]
 
 app.add_middleware(
@@ -44,21 +44,24 @@ app.add_middleware(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-queue_pool = [Queue(channel, connection=conn)
-                  for channel in ['high', 'default', 'low']]
-popularity_thresholds_500k_samples = [
-    2500, 2000, 1500, 1000, 700, 400, 250, 150]
+queue_pool = [Queue(channel, connection=conn) for channel in ["high", "default", "low"]]
+popularity_thresholds_500k_samples = [2500, 2000, 1500, 1000, 700, 400, 250, 150]
 
 
 # FROM_DOMAIN = "letterboxd-recommendations.herokuapp.com"
 # TO_DOMAIN = "letterboxd.samlearner.com"
 
-@app.get('/', response_class=HTMLResponse)
-def homepage():
-    return templates.TemplateResponse("index.html", {})
 
-@app.get('/get_recs')
-def get_recs(username: str, training_data_size: int, popularity_filter: int, data_opt_in: bool):
+@app.get("/", response_class=HTMLResponse)
+def homepage():
+    return RedirectResponse("https://letterboxd.samlearner.com")
+    # return templates.TemplateResponse("index.html", {})
+
+
+@app.get("/get_recs")
+def get_recs(
+    username: str, training_data_size: int, popularity_filter: int, data_opt_in: bool
+):
     if popularity_filter >= 0:
         popularity_threshold = popularity_thresholds_500k_samples[popularity_filter]
     else:
@@ -67,49 +70,82 @@ def get_recs(username: str, training_data_size: int, popularity_filter: int, dat
     num_items = 1400
 
     ordered_queues = sorted(
-        queue_pool, key=lambda queue: DeferredJobRegistry(queue=queue).count)
-    print([(q, DeferredJobRegistry(queue=q).count)
-            for q in ordered_queues])
+        queue_pool, key=lambda queue: DeferredJobRegistry(queue=queue).count
+    )
+    print([(q, DeferredJobRegistry(queue=q).count) for q in ordered_queues])
     q = ordered_queues[0]
 
-    job_get_user_data = q.enqueue(get_client_user_data, args=(
-        username, data_opt_in,), description=f"Scraping user data for {username} (sample: {training_data_size}, popularity_filter: {popularity_threshold}, data_opt_in: {data_opt_in})", result_ttl=45, ttl=200)
-    job_build_model = q.enqueue(build_client_model, args=(username, training_data_size, popularity_threshold, num_items,), depends_on=job_get_user_data,
-                                description=f"Building model for {username} (sample: {training_data_size}, popularity_filter: {popularity_threshold})", result_ttl=30, ttl=200)
+    job_get_user_data = q.enqueue(
+        get_client_user_data,
+        args=(
+            username,
+            data_opt_in,
+        ),
+        description=f"Scraping user data for {username} (sample: {training_data_size}, popularity_filter: {popularity_threshold}, data_opt_in: {data_opt_in})",
+        result_ttl=45,
+        ttl=200,
+    )
+    job_build_model = q.enqueue(
+        build_client_model,
+        args=(
+            username,
+            training_data_size,
+            popularity_threshold,
+            num_items,
+        ),
+        depends_on=job_get_user_data,
+        description=f"Building model for {username} (sample: {training_data_size}, popularity_filter: {popularity_threshold})",
+        result_ttl=30,
+        ttl=200,
+    )
 
-    return JSONResponse({
-        "redis_get_user_data_job_id": job_get_user_data.get_id(),
-        "redis_build_model_job_id": job_build_model.get_id()
-    })
+    return JSONResponse(
+        {
+            "redis_get_user_data_job_id": job_get_user_data.get_id(),
+            "redis_build_model_job_id": job_build_model.get_id(),
+        }
+    )
+
 
 @app.get("/results")
 def get_results(redis_build_model_job_id: str, redis_get_user_data_job_id: str):
     job_ids = {
         "redis_build_model_job_id": redis_build_model_job_id,
-        "redis_get_user_data_job_id": redis_get_user_data_job_id
+        "redis_get_user_data_job_id": redis_get_user_data_job_id,
     }
 
     job_statuses = {}
     for key, job_id in job_ids.items():
         try:
-            job_statuses[key.replace('_id', '_status')] = Job.fetch(
-                job_id, connection=conn).get_status()
+            job_statuses[key.replace("_id", "_status")] = Job.fetch(
+                job_id, connection=conn
+            ).get_status()
         except NoSuchJobError:
-            job_statuses[key.replace('_id', '_status')] = "finished"
+            job_statuses[key.replace("_id", "_status")] = "finished"
 
-    end_job = Job.fetch(
-        job_ids['redis_build_model_job_id'], connection=conn)
-    execution_data = {"build_model_stage": end_job.meta.get('stage')}
+    end_job = Job.fetch(job_ids["redis_build_model_job_id"], connection=conn)
+    execution_data = {"build_model_stage": end_job.meta.get("stage")}
 
     try:
-        user_job = Job.fetch(
-            job_ids['redis_get_user_data_job_id'], connection=conn)
-        execution_data |= {"num_user_ratings": user_job.meta.get(
-            'num_user_ratings'), "user_status": user_job.meta.get('user_status')}
+        user_job = Job.fetch(job_ids["redis_get_user_data_job_id"], connection=conn)
+        execution_data |= {
+            "num_user_ratings": user_job.meta.get("num_user_ratings"),
+            "user_status": user_job.meta.get("user_status"),
+        }
     except NoSuchJobError:
         pass
 
     if end_job.is_finished:
-        return JSONResponse(status_code=200, content={"statuses": job_statuses, "execution_data": execution_data, "result": end_job.result})
+        return JSONResponse(
+            status_code=200,
+            content={
+                "statuses": job_statuses,
+                "execution_data": execution_data,
+                "result": end_job.result,
+            },
+        )
     else:
-        return JSONResponse(status_code=202, content={"statuses": job_statuses, "execution_data": execution_data})
+        return JSONResponse(
+            status_code=202,
+            content={"statuses": job_statuses, "execution_data": execution_data},
+        )
