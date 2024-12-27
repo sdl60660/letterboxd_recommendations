9b194cb7752894a57f700a8b71264fdc71cb100d
Sam Learner <samlearner@Sams-MacBook-Pro.local>
2024-12-26 23:40:02 -0500

    trying updated version of worker (connection syntax was deprecated

--- worker.py
+++ worker.py
@@ -2,7 +2,7 @@ import os
 from urllib.parse import urlparse
 import redis
 # from redis import Redis
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
 # from rq.command import send_kill_horse_command
 
 
@@ -10,9 +10,12 @@ listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISCLOUD_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
+queues = [Queue(x, connection=conn) for x in listen]
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+    worker = Worker(queues, connection=conn)
+    worker.work()
+    # with Connection(conn):
+        # worker = Worker(map(Queue, listen))
+        # worker.work()
         # send_kill_horse_command(redis, worker.name)
