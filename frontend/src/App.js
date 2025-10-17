182c48a4db47fedccc2764062383b7fd3ff5c5f9
Sam Learner <learnersd@gmail.com>
2025-10-17 09:33:20 -0400

    removing down notice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
     return (
         <div className="App">
             {/* <Header /> */}
-            <DownNotice/>
+            {/* <DownNotice/> */}
             {/* <ContributionAsk /> */}
             <Intro />
 
