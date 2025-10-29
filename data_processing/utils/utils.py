4147181e1dab3da8ffbf16883abdd0c43c184a35
Sam Learner <learnersd@gmail.com>
2025-10-29 13:13:58 -0500

    one more item in exclude list

--- data_processing/utils/utils.py
+++ data_processing/utils/utils.py
@@ -8,4 +8,4 @@ def format_seconds(seconds):
     
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
-explicit_exclude_list = ['no-half-measures-creating-the-final-season-of-breaking-bad', 'twin-peaks', 'fullmetal-alchemist-brotherhood', 'monster-2004', 'cowboy-bebop', 'one-piece-fan-letter', 'avatar-spirits', 'twin-peaks-the-return', 'attack-on-titan-the-last-attack', 'attack-on-titan-the-final-chapters-special-2', 'attack-on-titan-the-final-chapters-special-1-2023', 'attack-on-titan-chronicle', 'frieren-beyond-journeys-end', 'tapping-the-wire', 'andor-a-disney-day-special-look']
+explicit_exclude_list = ['no-half-measures-creating-the-final-season-of-breaking-bad', 'twin-peaks', 'fullmetal-alchemist-brotherhood', 'monster-2004', 'cowboy-bebop', 'one-piece-fan-letter', 'avatar-spirits', 'twin-peaks-the-return', 'attack-on-titan-the-last-attack', 'attack-on-titan-the-final-chapters-special-2', 'attack-on-titan-the-final-chapters-special-1-2023', 'attack-on-titan-chronicle', 'frieren-beyond-journeys-end', 'tapping-the-wire', 'andor-a-disney-day-special-look', 'one-crazy-summer-a-look-back-at-gravity-falls']
