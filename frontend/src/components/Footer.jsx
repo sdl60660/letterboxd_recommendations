2527db4bfd9fe34fe6cb5b43fdd0759b474741fa
Sam Learner <learnersd@gmail.com>
2025-11-02 13:00:43 -0500

    updating Methodology/Notes in the footer, though I'll need to do another pass on this later I think

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -108,15 +108,15 @@ const Footer = (props) => {
         <p>
           A user's "star" ratings are scraped their Letterboxd profile and
           assigned numerical ratings from 1 to 10 (accounting for half stars).
-          Their ratings are then combined with a sample of ratings from the top
-          4000 most active users on the site to create a collaborative filtering
-          recommender model using singular value decomposition (SVD). All movies
-          in the full dataset that the user has not rated are run through the
-          model for predicted scores and the items with the top predicted scores
-          are returned. Due to constraints in time and computing power, the
-          maxiumum sample size that a user is allowed to select is 900,000
-          samples, though there are over five million ratings in the full
-          dataset from the top 4000 Letterboxd users alone.
+          Their ratings are then folded into a model that's been trained on a
+          sample of ratings from other users on the site to create a
+          collaborative filtering recommender model using singular value
+          decomposition (SVD). All movies in the full dataset that the user has
+          not rated are run through the model for predicted scores and the items
+          with the top predicted scores are returned. Due to constraints in time
+          and computing power, the maxiumum sample size that a user is allowed
+          to select contains about 5,000,000 ratings, though there are over 50
+          million ratings in the full dataset.
         </p>
       </section>
       <section>
@@ -127,17 +127,13 @@ const Footer = (props) => {
           similarities in rating patterns between other users and movies. I've
           found that it tends to recommend very popular movies often, regardless
           of an individual user's taste ("basically everyone who watches 12
-          Angry Men seems to like it, so why wouldn't you?"). To help counteract
-          that, I included a popularity filter that filters by how many times a
-          movie has been rated in the dataset, so that users can specifically
-          find more obscure recommendations. I've also found that it
-          occasionally just completely whiffs (I guess most people who watch
-          "Taylor Swift: Reputation Stadium Tour" do like it, but it's not
-          really my thing). I think that's just the nature of the beast, to some
-          extent, particularly when working with a relatively small sample.
-          It'll return 50 recommendations and that's usually enough to work with
-          if I'm looking for something to watch, even if there are a couple
-          misses here or there.
+          Angry Men seems to like it, so why wouldn't you?"). I've tried to tune
+          the model to prefer results that are a little more personalized the
+          expense of a bit of accuracy, but it does still sometimes recommend
+          items with high global scores that might not fit a user's particular
+          preferences. I've also found that it occasionally just completely
+          whiffs (I guess most people who watch "Taylor Swift: Reputation
+          Stadium Tour" do like it, but it's not really my thing).
         </p>
         <p>
           If you opted in to your ratings being included in the model data and
