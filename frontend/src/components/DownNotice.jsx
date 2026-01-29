b6958e0d33810a10706b8ed0312ad994283c4a11
Sam Learner <learnersd@gmail.com>
2026-01-29 12:51:45 -0500

    adding down notice

--- frontend/src/components/DownNotice.jsx
+++ frontend/src/components/DownNotice.jsx
@@ -5,12 +5,12 @@ const DownNotice = () => {
   return (
     <div className={"site-message down-notice"}>
       <p>
-        NOTE: As of August 31, 2025, some site dependencies have changed and the
-        site is currently not functioning as intended.
+        NOTE: As of January 28, 2026, some changes to the Letterboxd site are preventing this tool
+        from accessing their data and functioning as intended.
       </p>
       <p>
-        I'm working on fixing this ASAP, but unfortunately the site will be down
-        as long as you're seeing this message.
+        I'm working on fixing this ASAP, but unfortunately the site will be down as long as you're
+        seeing this message.
       </p>
     </div>
   );
