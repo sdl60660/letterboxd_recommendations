962a268ca017d151c02058195114dfe42d777eb9
Sam Learner <learnersd@gmail.com>
2026-02-02 18:10:30 -0500

    adding down notice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import Controls from "./components/Controls";
 import ProgressTracking from "./components/ProgressTracking";
 import Results from "./components/Results";
 import Footer from "./components/Footer";
-// import DownNotice from "./components/DownNotice";
+import DownNotice from "./components/DownNotice";
 // import ContributionAsk from './components/ContributionAsk'
 
 function App() {
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       {/* <Header /> */}
-      {/* <DownNotice/> */}
+      <DownNotice/>
       {/* <ContributionAsk /> */}
       <Intro />
 
