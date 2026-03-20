f95d4967db159dadd01e3f334457b6c92610c6f9
Sam Learner <learnersd@gmail.com>
2026-03-20 12:52:20 -0400

    taking down the downnotice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       {/* <Header /> */}
-      <DownNotice/>
+      {/* <DownNotice/> */}
       {/* <ContributionAsk /> */}
       <Intro />
 
